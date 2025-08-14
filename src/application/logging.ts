d7c0ca40db2d507cf7be3599fe7e6eb0e8f345bd
Muhammad Farhan N <najibfarhan3@gmail.com>
2025-08-14 10:18:44 +0700

    update logger

--- src/application/logging.ts
+++ src/application/logging.ts
@@ -6,28 +6,39 @@ import {
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 import { Request } from 'express';
+import * as pc from 'picocolors';
 import { logger } from '../utils/logger';
 
 @Injectable()
 export class RestLoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req: Request = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { method, url, body, query, params } = req;
 
-    let finalResBody;
-    if (body?.password) {
-      finalResBody = { ...body, password: '******' };
-    }
-    finalResBody = JSON.stringify(finalResBody);
+    const finalResBody: string = JSON.stringify(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      body?.password ? { ...body, password: '******' } : body
+    );
 
     const now = Date.now();
 
     return next.handle().pipe(
       tap(() => {
         const duration = Date.now() - now;
+        let parameter: string = '';
+        if (Object.keys(query).length > 0) {
+          parameter = `| query: ${JSON.stringify(query)} `;
+        }
+        if (Object.keys(params).length > 0) {
+          parameter = parameter + `| params: ${JSON.stringify(params)} `;
+        }
+        if (finalResBody) {
+          parameter = parameter + `| body: ${finalResBody} `;
+        }
 
         logger.info(
-          `${method} ${url} | query: ${JSON.stringify(query)} | params: ${JSON.stringify(params)} | body: ${finalResBody} | response-time: ${duration}ms`
+          `${method} ${pc.yellow(url)} ${parameter}| response-time: ${duration}ms`
         );
       })
     );
